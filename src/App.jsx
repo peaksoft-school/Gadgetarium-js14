778a4db341a35822c221c2d08844e35b3da817b2
Bayel-99 <vetrovlena42@gmail.com>
2024-10-05 14:20:33 +0600

    Ui-button

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
-import React from "react";
-
+import React from 'react';
+import Button from './components/UI/Button';
+import Shop from './assets/icon/Shop.svg';
 const App = () => {
-  return <h1>Gadgetarium</h1>;
+  return (
+    <div>
+      <h1>Gadgetarium</h1>
+    </div>
+  );
 };
 
 export default App;
