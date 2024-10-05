1b4f99ed9a7d98ba7bd719786fa3c68c90326033
Kutman <kubanchbekovkutman9@gmail.com>
2024-10-05 13:27:10 +0600

    ..

--- src/components/UI/Input.jsx
+++ src/components/UI/Input.jsx
@@ -18,7 +18,7 @@ const Input = forwardRef(
     return (
       <LabelDiv>
         <Typography
-          sx={{ color: disabled ? 'lightgray' : '#939292', margin: 0 }} 
+          sx={{ color: disabled ? "lightgray" : "#939292", margin: 0 }}
         >
           {label}
         </Typography>
@@ -33,9 +33,7 @@ const Input = forwardRef(
           disabled={disabled}
           InputProps={{
             startAdornment: Icon && (
-              <InputAdornment position="start">
-                {Icon}
-              </InputAdornment>
+              <InputAdornment position="start">{Icon}</InputAdornment>
             ),
           }}
           {...props}
@@ -50,11 +48,11 @@ export default Input;
 const StyledInput = styled(TextField)(({ theme, error }) => ({
   width: "100%",
   borderRadius: "8px",
-  margin: 0, 
-  padding: 0, 
+  margin: 0,
+  padding: 0,
 
   "& .MuiOutlinedInput-root": {
-    padding: 0, 
+    padding: 0,
     "& fieldset": {
       borderColor: error ? theme.palette.error.main : "#E5E5E5",
     },
@@ -62,20 +60,20 @@ const StyledInput = styled(TextField)(({ theme, error }) => ({
       borderColor: "#cb11ab",
     },
     "&.Mui-focused fieldset": {
-      borderColor: "#c9ccd4", 
+      borderColor: "#c9ccd4",
       borderWidth: "3px",
     },
   },
 
   "& .MuiOutlinedInput-input": {
-    padding: "8px", 
-    margin: 0, 
+    padding: "8px",
+    margin: 0,
   },
 }));
 
 const LabelDiv = styled("div")({
   display: "flex",
   flexDirection: "column",
-  margin: 0, 
-  padding: 0, 
+  margin: 0,
+  padding: 0,
 });
