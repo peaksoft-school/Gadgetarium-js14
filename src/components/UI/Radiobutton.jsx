80c5f56ff6d71590dd8c295ecf9db28a8d88aa22
Bayel-99 <vetrovlena42@gmail.com>
2024-10-10 13:24:01 +0600

    save

--- src/components/UI/Radiobutton.jsx
+++ src/components/UI/Radiobutton.jsx
@@ -1,11 +1,44 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { Radio, RadioGroup, FormControlLabel } from '@mui/material';
+import { styled } from '@mui/system';
 
-const Radiobutton = ({options}) => {
+const RadioButton = ({ options }) => {
+  const [selectedOption, setSelectedOption] = useState(null);
+
+  const handleChange = (event) => {
+    const value = event.target.value;
+
+    setSelectedOption(selectedOption === value ? null : value);
+  };
+
   return (
-    <div>
-      
-    </div>
-  )
-}
+    <RadioGroup value={selectedOption} onChange={handleChange}>
+      {options.map((option) => (
+        <FormControlLabel
+          key={option.value}
+          control={<StyledRadio value={option.value} />}
+          label={option.label}
+          sx={{
+            '& .MuiFormControlLabel-label': {
+              color: 'gray',
+            },
+            '&:hover .MuiFormControlLabel-label': {
+              color: '#cb11ab',
+            },
+          }}
+        />
+      ))}
+    </RadioGroup>
+  );
+};
 
-export default Radiobutton
+const StyledRadio = styled(Radio)(({ theme }) => ({
+  '&.Mui-checked': {
+    color: '#cb11ab',
+  },
+  '&:hover': {
+    backgroundColor: 'transparent',
+  },
+}));
+
+export default RadioButton;
