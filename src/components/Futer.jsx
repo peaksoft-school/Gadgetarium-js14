fec05ee27f6194fa6ca052e6bdbf87908f47f1a5
Kutman <kubanchbekovkutman9@gmail.com>
2024-10-10 19:44:47 +0600

    footer-project

--- src/components/Futer.jsx
+++ src/components/Futer.jsx
@@ -3,6 +3,29 @@ import { Box, styled } from "@mui/material"
 const Futer = () => {
   return (
     <ConteinerFuter className="futerConteiner">
+        <Box className='box-text'>
+            <p>Каталог</p>
+            <p>Смартфоны</p>
+            <p>Ноутбуки и планшеты</p>
+            <p>Смарт-часы и браслеты</p>
+            <p>Аксессуары </p>
+        </Box>
+        <Box className='box-text'>
+            <p>Будь с нами</p>
+            <p>Акции</p>
+            <p>Новинки</p>
+            <p>Популярные категории </p>
+        </Box>
+        <Box className='box-text'>
+            <p>Помощь и сервисы</p>
+            <p>О магазине</p>
+            <p>Доставка</p>
+            <p>FAQ</p>
+            <p>Контакты</p>
+        </Box>
+        <Box className='box-text'>
+            <p>Расскажем об акциях и скидках</p>
+        </Box>
     </ConteinerFuter>
   )
 }
@@ -12,8 +35,25 @@ export default Futer
 const ConteinerFuter = styled(Box)(({ theme }) => ({
   // Стиль для класса "futerConteiner"
   '&.futerConteiner': {
-    win
+    width:'100%',
+    display:'flex',
+    justifyContent:'space-around',
+
     backgroundColor: theme.palette.black.dark,
-     // Используйте правильное свойство палитры
+  },
+  '&  .box-text' :{
+    display: 'flex',
+    flexDirection: "column",
+    
+    
+    
+    
+    padding: '20px',
+    '& p':{
+      color: theme.palette.lightGrey.light,
+      margin: '10px',
+      fontSize: '16px',
+
+    }
   }
 }))
