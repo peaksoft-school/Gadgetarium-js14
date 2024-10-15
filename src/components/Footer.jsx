5a9579e6f67b5a1998ef600aa39325b066c5fdba
Kutman <kubanchbekovkutman9@gmail.com>
2024-10-15 16:47:27 +0600

    Fixed the error

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,6 @@
 // eslint-disable-next-line no-unused-vars
 import React from "react";
-import { Box,  styled, TextField, Typography } from "@mui/material";
-// import Input from "./UI/Input";
+import { Box, styled, TextField, Typography } from "@mui/material";
 import Button from "./UI/Button";
 import {
   Chasy,
@@ -15,101 +14,94 @@ const Footer = () => {
   return (
     <>
       <ConteinerFuter className="futerConteiner">
-        <div style={{ display: "flex", width: "1300px", margin: "0 auto" }}>
-          <div
-            style={{
-              width: "900px",
-              display: "flex",
-            }}
-          >
-            <Box className="box-text">
-              <FooterTitle>Каталог</FooterTitle>
-              <FooterLinkp>Смартфоны</FooterLinkp>
-              <FooterLinkp>Ноутбуки и планшеты</FooterLinkp>
-              <FooterLinkp>Смарт-часы и браслеты</FooterLinkp>
-              <FooterLinkp>Аксессуары</FooterLinkp>
-            </Box>
-            <Box className="box-text">
-              <FooterTitle>Будь с нами</FooterTitle>
-
-              <FooterLinkp>Акции</FooterLinkp>
-              <FooterLinkp>Новинки</FooterLinkp>
-              <FooterLinkp>Популярные категории</FooterLinkp>
-            </Box>
-            <Box className="box-text">
-              <FooterTitle>Помощь и сервисы</FooterTitle>
-              <FooterLinkp>О магазине</FooterLinkp>
-              <FooterLinkp>Доставка</FooterLinkp>
-              <FooterLinkp>FAQ</FooterLinkp>
-              <FooterLinkp>Контакты</FooterLinkp>
-            </Box>
-          </div>
-
-          <Box className="box-text2">
+        <StyledCond>
+          <Box className="inputcrovs">
             <div
               style={{
+                width: "900px",
                 display: "flex",
-                flexDirection: "column",
-                gap: "10px",
-                marginBottom: "20px",
               }}
             >
-              <FooterTitle style={{position:'relative',top:'20px'}}>Расскажем об акциях и скидках</FooterTitle>
-              <Box className="button-input">
-                <TextField
-                  size="small"
-                  className="inputr"
-                  sx={{ borderRadius: "4px 0 0 4px" }}
-                  placeholder="Email"
-                />
-                <Button
-                  className="button"
-                  variant="contained"
-                  style={{ borderRadius: "0 4px 4px 0" }}
-                >
-                  Подписаться
-                </Button>
-              </Box>
-              <p>
-                Нажимая на кнопку «подписаться», Вы соглашаетесь <br /> на
-                обработку персональных данных
-              </p>
+              <StyledBox>
+                <Box className="box-text">
+                  <FooterTitle>Каталог</FooterTitle>
+                  <FooterLinkp>Смартфоны</FooterLinkp>
+                  <FooterLinkp>Ноутбуки и планшеты</FooterLinkp>
+                  <FooterLinkp>Смарт-часы и браслеты</FooterLinkp>
+                  <FooterLinkp>Аксессуары</FooterLinkp>
+                </Box>
+                <Box className="box-text">
+                  <FooterTitle>Будь с нами</FooterTitle>
+
+                  <FooterLinkp>Акции</FooterLinkp>
+                  <FooterLinkp>Новинки</FooterLinkp>
+                  <FooterLinkp>Популярные категории</FooterLinkp>
+                </Box>
+                <Box className="box-text">
+                  <FooterTitle>Помощь и сервисы</FooterTitle>
+                  <FooterLinkp>О магазине</FooterLinkp>
+                  <FooterLinkp>Доставка</FooterLinkp>
+                  <FooterLinkp>FAQ</FooterLinkp>
+                  <FooterLinkp>Контакты</FooterLinkp>
+                </Box>
+              </StyledBox>
             </div>
-            <Box className="text-container">
-              <br />
-              <StyledButton>
-                <Box className="contact-info">
-                  <img src={Telefon} alt="Phone" />
-                  <Typography className="text">+996 (400) 00 00 00</Typography>
+
+            <Box className="box-text2">
+              <div>
+                <FooterTitle>Расскажем об акциях и скидках</FooterTitle>
+                <Box className="button-input">
+                  <TextField
+                    label="Email"
+                    size="small"
+                    className="inputr"
+                    sx={{ borderRadius: "4px 0 0 4px" }}
+                    placeholder="Email"
+                  />
+                  <Button
+                    className="button"
+                    variant="contained"
+                    style={{ borderRadius: "0 4px 4px 0" }}
+                  >
+                    Подписаться
+                  </Button>
                 </Box>
-                <Box className="contact-info">
-                  <img src={Pochta} alt="Email" />
-                  <Typography className="text">Gadgetarium.kg</Typography>
-                </Box>
-                <Box className="contact-info">
-                  <img src={Mestonahajdenie} alt="Location" />
-                  <Typography className="text">
-                    г.Бишкек, ул. Гражданская 119
-                  </Typography>
-                </Box>
-                <Box className="contact-info">
-                  <img src={Chasy} alt="Hours" />
-                  <Typography className="text">
-                    С 10:00 до 21:00 (без выходных)
-                  </Typography>
-                </Box>
-              </StyledButton>
+
+                <p style={{ position: "relative", top: "10px" }}>
+                  Нажимая на кнопку «подписаться», Вы соглашаетесь <br /> на
+                  обработку персональных данных
+                </p>
+              </div>
+              <Box className="text-container">
+                <br />
+                <StyledButton>
+                  <Box className="contact-info">
+                    <img src={Telefon} alt="Phone" />
+                    <FooterText>+996 (400) 00 00 00</FooterText>
+                  </Box>
+                  <Box className="contact-info">
+                    <img src={Pochta} alt="Email" />
+                    <FooterText>Gadgetarium.kg</FooterText>
+                  </Box>
+                  <Box className="contact-info">
+                    <img src={Mestonahajdenie} alt="Location" />
+                    <FooterText>г.Бишкек, ул. Гражданская 119</FooterText>
+                  </Box>
+                  <Box className="contact-info">
+                    <img src={Chasy} alt="Hours" />
+                    <FooterText>С 10:00 до 21:00 (без выходных)</FooterText>
+                  </Box>
+                </StyledButton>
+              </Box>
             </Box>
           </Box>
-        </div>
+        </StyledCond>
         <Box className="hr"></Box>
-        <BoxZagalovok
-          className="boxzagalovak"
-        >
+        <BoxZagalovok className="boxzagalovak">
           <img src={Nathvania} alt="" />
-          <Typography style={{color:'#6c7386'}} textAlign={"center"} lineHeight={1.5} m={"10px 0 0 0"}>
+          <StyledTypografy>
             © 2022 Gadgetarium. Интернет магазин <br /> Все права защищены.
-          </Typography>
+          </StyledTypografy>
         </BoxZagalovok>
       </ConteinerFuter>
     </>
@@ -118,15 +110,31 @@ const Footer = () => {
 
 export default Footer;
 
+const StyledCond = styled(Box)(() => ({
+  display: "flex",
+  justifyContent: "space-between",
+  padding: "0px 30px 0 30px",
+}));
+
+const StyledTypografy = styled(Typography)(() => ({
+  color: "#6c7386",
+  textAlign: "center",
+  lineHeight: 1.5,
+}));
+
 const StyledButton = styled(Box)(() => ({
   display: "flex",
   flexDirection: "column",
-  gap: "10px",
+  position: "relative",
+  top: "10px",
+  gap: "5px",
   "& .contact-info": {
     display: "flex",
     alignItems: "center",
     gap: "10px",
-    color: "#d3d3d3",
+  },
+  "& .text": {
+    color: "#6d7588",
   },
   "& img": {
     width: "20px",
@@ -134,21 +142,31 @@ const StyledButton = styled(Box)(() => ({
   },
 }));
 
+const StyledBox = styled(Box)(() => ({
+  display: "flex",
+  justifyContent: "space-between",
+  width: "100%",
+  margin: "0 auto",
+  color: "#80899e",
+
+}));
+
 const ConteinerFuter = styled(Box)(() => ({
   "& .box-text2": {
-    width: "45%",
+    flexDirection: "column",
   },
   "&.futerConteiner": {
-    width: "100%",
-    backgroundColor: "#1c1c1c",
+    backgroundColor: "#1a1a25",
     padding: "20px",
     color: "#d3d3d3",
   },
-  "& .box-text": {
+  "& .inputcrovs": {
     display: "flex",
-    flexDirection: "column",
-    padding: "20px",
-    color:'#80899e',
+    justifyContent: "space-between",
+    width: "100%",
+    margin: "0 auto",
+  },
+  "& .box-text": {
     "& .textBox": {
       color: "#d3d3d3",
       margin: "10px",
@@ -180,12 +198,7 @@ const ConteinerFuter = styled(Box)(() => ({
       height: "40px",
     },
   },
-  "& .text-container": {
-    "& p": {
-      color: "#d3d3d3",
-      fontSize: "12px",
-    },
-  },
+
   "& .hr": {
     border: "1px solid #2a2c38",
     height: "2px",
@@ -198,7 +211,6 @@ const ConteinerFuter = styled(Box)(() => ({
 }));
 
 const BoxZagalovok = styled(Box)(() => ({
-  width: "1300px",
   margin: "0 auto",
   display: "flex",
   justifyContent: "center",
@@ -209,13 +221,21 @@ const FooterTitle = styled(Typography)({
   fontWeight: 600,
   fontSize: "18px",
   marginBottom: "16px",
-  color: "#ffffff",
+  color: "#fff",
 });
 const FooterLinkp = styled(Typography)({
   fontSize: "14px",
   cursor: "pointer",
-  color: "#d3d3d3",
-  lineHeight: '30px',
+  color: "#464a59",
+  lineHeight: "30px",
+  "&:hover": {
+    color: "#ffffff",
+  },
+});
+
+const FooterText = styled(Typography)({
+  color: "#464a59",
+  fontSize: "15px",
   "&:hover": {
     color: "#ffffff",
   },
