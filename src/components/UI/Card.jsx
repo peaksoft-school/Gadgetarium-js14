b1b268c5939fc7a28e506176dfb2ff58fe5f026b
Kutman <kubanchbekovkutman9@gmail.com>
2024-10-15 19:53:33 +0600

    ...

--- src/components/UI/Card.jsx
+++ src/components/UI/Card.jsx
@@ -1,8 +1,8 @@
 /* eslint-disable no-unused-vars */
 import styled from "@emotion/styled";
 import { Box, Button, Typography } from "@mui/material";
-import StarIcon from "@mui/icons-material/Star"; // Иконка полной звезды
-import StarHalfIcon from "@mui/icons-material/StarHalf"; // Иконка полузаполненной звезды
+import StarIcon from "@mui/icons-material/Star";
+import StarHalfIcon from "@mui/icons-material/StarHalf";
 import React from "react";
 import { Component, greyHeart, GroceryCart } from "../../assets/icon";
 
@@ -19,12 +19,8 @@ const Card = ({
   discountClas,
   ...rest
 }) => {
-  const fullStars = Math.floor(reiting); 
-  const hasHalfStar = reiting % 1 !== 0; 
-
-  
-
-  
+  const fullStars = Math.floor(reiting);
+  const hasHalfStar = reiting % 1 !== 0;
 
   return (
     <StyledContainer>
@@ -35,44 +31,37 @@ const Card = ({
         </StyledIcanConteiner>
 
         <Box>
-        {/* Проверка иконки скидки (discountIcon), если строка не пустая */}
-        {discount ? (
-          <img
-            className="scitca"
-            src={discount}
-            alt="Discount Icon"
-            style={{ position: "relative", top: "-35px", padding: "5px" }}
-          />
-        ) : null}
+          {discount ? (
+            <img
+              className="scitca"
+              src={discount}
+              alt="Discount Icon"
+              style={{ position: "relative", top: "-35px", padding: "5px" }}
+            />
+          ) : null}
 
-        {/* Проверка на видимость иконки 1 с тернарным оператором */}
-        {discountNew ? (
-          <img
-            className="scitca"
-            src={discountNew}
-            alt="Icon One"
-            style={{ position: "relative", top: "-35px", padding: "5px" }}
-          />
-        ) : null}
+          {discountNew ? (
+            <img
+              className="scitca"
+              src={discountNew}
+              alt="Icon One"
+              style={{ position: "relative", top: "-35px", padding: "5px" }}
+            />
+          ) : null}
 
-        {/* Проверка на видимость иконки 2 с тернарным оператором */}
-        {discountClas ? (
-          <img
-            className="scitca"
-            src={discountClas}
-            alt="Icon Two"
-            style={{ position: "relative", top: "-35px", padding: "5px" }}
-          />
-        ) : null}
-
-        {/* Остальной контент карточки */}
-     
-      </Box>
+          {discountClas ? (
+            <img
+              className="scitca"
+              src={discountClas}
+              alt="Icon Two"
+              style={{ position: "relative", top: "-35px", padding: "5px" }}
+            />
+          ) : null}
+        </Box>
         <ImageContainer>
           <img className="img" src={img} alt={text} />
         </ImageContainer>
 
-        {/* Тело карточки */}
         <Box padding={2}>
           <Availability>{`В наличии (${title})`}</Availability>
           <ProductName>{text}</ProductName>
@@ -91,15 +80,16 @@ const Card = ({
             </Typography>
 
             <div style={{ display: "flex", flexDirection: "row" }}>
-              {/* Рендерим полные звезды */}
               {[...Array(fullStars)].map((_, i) => (
                 <StarIcon key={i} style={{ color: "#FFC107" }} />
               ))}
               {hasHalfStar && <StarHalfIcon style={{ color: "#FFC107" }} />}
-              {/* Рендерим пустые звезды до 5 */}
-                {[...Array(5 - fullStars - (hasHalfStar ? 1 : 0))].map((_, i) => (
-                  <StarIcon key={i + fullStars + (hasHalfStar ? 1 : 0)} style={{ color: "#909cb5" }} />
-                ))}
+              {[...Array(5 - fullStars - (hasHalfStar ? 1 : 0))].map((_, i) => (
+                <StarIcon
+                  key={i + fullStars + (hasHalfStar ? 1 : 0)}
+                  style={{ color: "#909cb5" }}
+                />
+              ))}
             </div>
 
             <Typography variant="body2" color="textSecondary">
@@ -107,7 +97,6 @@ const Card = ({
             </Typography>
           </RatingContainer>
 
-          {/* Цена и кнопка */}
           <StyledBoxProject>
             <Box>
               <NewPrice>{newPrice}</NewPrice>
@@ -125,13 +114,13 @@ const Card = ({
 
 export default Card;
 
-const StyledContainer = styled(Box)( {
+const StyledContainer = styled(Box)({
   display: "flex",
   justifyContent: "center",
   gap: "16px",
 });
 
-const StyledIcanConteiner = styled(Box)( {
+const StyledIcanConteiner = styled(Box)({
   display: "flex",
   justifyContent: "end",
   padding: "5px",
@@ -139,7 +128,7 @@ const StyledIcanConteiner = styled(Box)( {
   zIndex: 1,
 });
 
-const StyledBoxProject = styled(Box)(() => ( {
+const StyledBoxProject = styled(Box)(() => ({
   display: "flex",
   justifyContent: "space-between",
   alignItems: "center",
@@ -163,7 +152,7 @@ const StyledBoxProject = styled(Box)(() => ( {
   },
 }));
 
-const StyledCard = styled(Box)( {
+const StyledCard = styled(Box)({
   width: "300px",
   border: "1px solid #e0e0e0",
   borderRadius: "12px",
@@ -172,7 +161,7 @@ const StyledCard = styled(Box)( {
   position: "relative",
 });
 
-const ImageContainer = styled(Box)( {
+const ImageContainer = styled(Box)({
   display: "flex",
   justifyContent: "center",
   alignItems: "center",
@@ -188,20 +177,20 @@ const ImageContainer = styled(Box)( {
   },
 });
 
-const Availability = styled(Typography)( {
+const Availability = styled(Typography)({
   color: "#4CAF50",
   fontWeight: "bold",
   fontSize: "12px",
   marginBottom: "8px",
 });
 
-const ProductName = styled(Typography)( {
+const ProductName = styled(Typography)({
   fontWeight: "bold",
   fontSize: "16px",
   marginBottom: "8px",
 });
 
-const RatingContainer = styled(Box)( {
+const RatingContainer = styled(Box)({
   display: "flex",
   alignItems: "center",
   gap: "4px",
@@ -209,13 +198,13 @@ const RatingContainer = styled(Box)( {
   position: "relative",
 });
 
-const OldPrice = styled(Typography)( {
+const OldPrice = styled(Typography)({
   color: "#9e9e9e",
   textDecoration: "line-through",
   fontSize: "14px",
 });
 
-const NewPrice = styled(Typography)( {
+const NewPrice = styled(Typography)({
   color: "#000",
   fontWeight: "bold",
   fontSize: "18px",
