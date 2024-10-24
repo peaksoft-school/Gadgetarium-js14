5d165c5212a7a1503a10e791bf69a431445b3510
Bayel-99 <vetrovlena42@gmail.com>
2024-10-24 16:48:27 +0600

    change-delivery

--- src/layout/delivery/Delivery.jsx
+++ src/layout/delivery/Delivery.jsx
@@ -3,90 +3,18 @@ import { Box } from '@mui/system';
 import {
   Component44,
   FrameCard,
-  Fb,
-  Gadgettarium,
-  GroceryCartTwo,
-  Group,
-  Heart,
-  ico,
-  Instagram,
   Money,
-  ScalesEight,
-  Systema,
+  ico,
   Wallet,
-  WhatsApp,
 } from '../../assets/icon/index';
 import { styled } from '@mui/material/styles';
-import { TextField, InputAdornment } from '@mui/material';
 import Footer from '../../components/Footer';
-import SidebarMenu from '../../components/UI/SaidebarMenu';
+import Header from '../../components/Header';
 
 const Delivery = () => {
   return (
     <PapaDiv>
-      <header>
-        <StyledDiv>
-          <img src={Gadgettarium} alt="" style={{ height: '45px' }} />
-          <DivStyle>
-            <p>Главная</p>
-            <p>О Магазине</p>
-            <p>Доставка</p>
-            <p>FAG</p>
-            <p>Контакты</p>
-          </DivStyle>
-          <StyledNumber>
-            +996 (700) 18-09-19
-            <img src={Group} alt="" style={{ width: '20px', height: '20px' }} />
-          </StyledNumber>
-        </StyledDiv>
-        <StyledDiv>
-          <FlexContainer>
-            <SidebarMenu />
-            <StyledInput
-              id="myInput"
-              label="Поиск по каталогу магазина"
-              variant="outlined"
-              InputProps={{
-                endAdornment: (
-                  <InputAdornment position="end">
-                    <img
-                      src={Systema}
-                      alt="search icon"
-                      style={{ width: '20px', height: '20px' }}
-                    />
-                  </InputAdornment>
-                ),
-              }}
-            />
-          </FlexContainer>
-          <SocialIcons>
-            <img src={Fb} alt="" style={{ width: '25px', height: '25px' }} />
-            <img
-              src={WhatsApp}
-              alt=""
-              style={{ width: '25px', height: '25px' }}
-            />
-            <img
-              src={Instagram}
-              alt=""
-              style={{ width: '20px', height: '20px' }}
-            />
-          </SocialIcons>
-          <CartIcons>
-            <img
-              src={ScalesEight}
-              alt=""
-              style={{ width: '25px', height: '25px' }}
-            />
-            <img src={Heart} alt="" style={{ width: '17px', height: '17px' }} />
-            <img
-              src={GroceryCartTwo}
-              alt=""
-              style={{ width: '27px', height: '27px' }}
-            />
-          </CartIcons>
-        </StyledDiv>
-      </header>
+      <Header />
       <Becgraund>
         <Divider>
           <StyledBlock>
@@ -96,7 +24,7 @@ const Delivery = () => {
           <Block>
             <h1 style={{ fontSize: '29px' }}>Доставка</h1>
           </Block>
-          <Styledhr></Styledhr>
+          <Styledhr />
         </Divider>
         <div>
           <CityDeliveryStyled>
@@ -128,21 +56,21 @@ const Delivery = () => {
               </DivBlockRow>
               <PaymentDelivery>
                 <PaymentRow>
-                  <img src={Component44} alt="" />
+                  <img src={Component44} alt="Оплата картой онлайн" />
                   <p>
                     Оплата картой <br />
                     онлайн
                   </p>
                 </PaymentRow>
                 <PaymentRow>
-                  <img src={Money} alt="" />
+                  <img src={Money} alt="Наличные при получении" />
                   <p>
                     Наличные при <br />
                     получении
                   </p>
                 </PaymentRow>
                 <PaymentRow>
-                  <img src={FrameCard} alt="" />
+                  <img src={FrameCard} alt="Картой при получении" />
                   <p>
                     Картой <br />
                     при получении
@@ -161,14 +89,14 @@ const Delivery = () => {
 const DeliveryOption = ({ title, description }) => (
   <DeliveryOptionStyled>
     <Row>
-      <img src={ico} alt="" />
+      <Icon src={ico} alt="" />
       <TextBlock>
         <p>{title}</p>
         <p>{description}</p>
       </TextBlock>
     </Row>
     <Row>
-      <img src={Wallet} alt="" />
+      <Icon src={Wallet} alt="" />
       <TextBlock>
         <p className="no-bold">Предоплата не требуется</p>
       </TextBlock>
@@ -176,80 +104,19 @@ const DeliveryOption = ({ title, description }) => (
   </DeliveryOptionStyled>
 );
 
+// Стили
 const PapaDiv = styled(Box)(({ theme }) => ({
   boxSizing: 'border-box',
   margin: '0 auto',
 }));
+
 const Becgraund = styled(Box)(({ theme }) => ({
   backgroundColor: '#f4f4f4',
 }));
 
-const StyledDiv = styled(Box)(({ theme }) => ({
-  display: 'flex',
-  justifyContent: 'space-between',
-  alignItems: 'center',
+const Divider = styled(Box)(({ theme }) => ({
   padding: '10px 95px',
-  backgroundColor: '#1a1a25',
   flexWrap: 'wrap',
-}));
-
-const FlexContainer = styled(Box)(({ theme }) => ({
-  display: 'flex',
-  alignItems: 'center',
-  gap: '100px',
-}));
-
-const DivStyle = styled(Box)(({ theme }) => ({
-  display: 'flex',
-  alignItems: 'center',
-  gap: '10px',
-  color: 'white',
-  cursor: 'pointer',
-  fontSize: '14px',
-}));
-
-const StyledInput = styled(TextField)(({ theme }) => ({
-  '& .MuiOutlinedInput-root': {
-    borderRadius: '10px',
-    height: '45px',
-    width: '687px',
-    display: 'flex',
-    alignItems: 'center',
-    color: 'white',
-    border: '1px solid white',
-    zIndex: '0',
-  },
-  '& .MuiOutlinedInput-input': {
-    padding: '0 14px',
-    textAlign: 'center',
-    color: 'white',
-  },
-  '& .MuiInputLabel-root': {
-    color: 'white',
-    fontWeight: 500,
-  },
-}));
-
-const StyledNumber = styled(Box)(({ theme }) => ({
-  display: 'flex',
-  alignItems: 'center',
-  gap: '10px',
-  fontSize: '18px',
-  fontWeight: 500,
-  color: 'white',
-  cursor: 'pointer',
-}));
-
-const SocialIcons = styled(Box)(({ theme }) => ({
-  display: 'flex',
-  alignItems: 'center',
-  gap: '5px',
-}));
-
-const CartIcons = styled(Box)(({ theme }) => ({
-  display: 'flex',
-  alignItems: 'center',
-  gap: '5px',
 }));
 
 const StyledBlock = styled(Box)(({ theme }) => ({
@@ -259,16 +126,11 @@ const StyledBlock = styled(Box)(({ theme }) => ({
   cursor: 'pointer',
   '& p': {
     transition: 'color 0.3s ease',
-    margin: '0', // Убираем отступы
+    margin: '0',
+    '&:hover': {
+      color: 'grey', 
+    },
   },
-  '& p:hover': {
-    color: 'grey', // Цвет при наведении
-  },
-}));
-
-const Divider = styxled(Box)(({ theme }) => ({
-  padding: '10px 95px',
-  flexWrap: 'wrap',
 }));
 
 const Block = styled(Box)(({ theme }) => ({
@@ -303,18 +165,28 @@ const Row = styled(Box)(({ theme }) => ({
   display: 'flex',
   alignItems: 'center',
   gap: '10px',
+  cursor: 'pointer',
+  '&:hover': {
+    opacity: 0.7,
+    '& img': {
+      opacity: 0.7, 
+    },
+    '& p': {
+      color: 'grey', 
+    },
+  },
 }));
 
 const TextBlock = styled(Box)(({ theme }) => ({
   display: 'flex',
   flexDirection: 'column',
   justifyContent: 'flex-start',
-  cursor: 'pointer',
-  '& p:first-of-type:not(.no-bold)': {
+  '& p:first-of-type': {
     fontWeight: 'bold',
   },
   '& p': {
-    margin: 0, // Убираем отступы
+    margin: 0,
+    transition: 'color 0.3s ease',
   },
 }));
 
@@ -332,9 +204,23 @@ const PaymentRow = styled(Box)(({ theme }) => ({
   padding: '5px',
   marginBottom: '70px',
   cursor: 'pointer',
+  '&:hover': {
+    opacity: 0.7,
+    '& img': {
+      opacity: 0.7,
+    },
+    '& p': {
+      color: 'grey',
+    },
+  },
 }));
+
 const DivBlockRow = styled(Box)(({ theme }) => ({
   marginTop: '70px',
-}))  
+}));
+
+const Icon = styled('img')(({ theme }) => ({
+  cursor: 'pointer',
+}));
 
 export default Delivery;
