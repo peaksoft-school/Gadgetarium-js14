8b1f4e8db172e9b59cbc65a0edb2e40ec416f876
Bayel-99 <vetrovlena42@gmail.com>
2024-10-25 17:45:01 +0600

    create-admin-header

---
+++ src/components/UI/AdminHeader.jsx
@@ -0,0 +1,10 @@
+const AdminHeader = () => {
+  return <div>
+    <header>
+      <div>
+        <div><img src={Gadgettarium} alt="" /></div>
+      </div>
+    </header>
+  </div>;
+};
+export default AdminHeader;
