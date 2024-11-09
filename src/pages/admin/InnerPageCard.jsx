0c222ad10e2e636cfa23153dcc46c136400510d5
Bayel-99 <vetrovlena42@gmail.com>
2024-11-09 17:47:27 +0600

    create

---
+++ src/pages/admin/InnerPageCard.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+// import AdminHeader from '../../components/UI/AdminHeader';
+
+const InnerPageCard = () => {
+  return (
+    <div>
+      {/* <AdminHeader /> */}
+    </div>
+  );
+};
+
+export default InnerPageCard;
