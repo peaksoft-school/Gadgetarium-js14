8c59c2fac60c3f93fa9bf1d49602fb9338e56e17
Kutman <kubanchbekovkutman9@gmail.com>
2024-11-29 15:54:05 +0600

    ..

--- src/store/store.js
+++ src/store/store.js
@@ -1,12 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { authSlice } from "./auth/authSlice";
 import { productAdminSlice } from "./productAdmin/productAdminSlice";
+import { infografictSlice } from "./productAdmin/infografictSlice";
 // import { bannersSlice } from "./productAdmin/bannerSlice";
 
 export const store = configureStore({
   reducer: {
     [authSlice.name]: authSlice.reducer,
     [productAdminSlice.name]: productAdminSlice.reducer,
-    // [bannersSlice.name]:bannersSlice.reducer
+    [infografictSlice.name]:infografictSlice.reducer
   },
 });
