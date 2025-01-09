07d0825448abc5f2956b2f6ceaa0a3abbe2ee3c9
Kutman <kubanchbekovkutman9@gmail.com>
2025-01-09 16:32:07 +0600

    ..

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,7 @@
-<<<<<<< HEAD
-=======
+import React from "react";
 import AppRouter from "./routes/Approuter";
 
 const App = () => {
   return <AppRouter />;
 };
->>>>>>> c8df8344d743a49e7cc4a2d73fc2fc453a9e976f
-
-const App = () => {
-  return (
-    <div>App</div>
-  )
-}
-
 export default App
